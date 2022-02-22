e3b63d6f34241c918306a8096676fa4c9f22223a
wugelis <wergelis@msn.com>
2022-02-22 13:29:20 +0800

    初次簽入

---
+++ BlazorSvrBtnLab1Net6/Components/MyButton2.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorSvrBtnLab1Net6.Components
+{
+    public partial class MyButton2
+    {
+        [Parameter] public string ButtonText { get; set; }
+    }
+}
---
+++ BlazorSvrBtnLab1Net6/Components/MyButton3Class.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorSvrBtnLab1Net6.Components
+{
+    public class MyButton3Class: ComponentBase
+    {
+        [Parameter] public string ButtonText { get; set; }
+
+        [Parameter] public EventCallback<MouseEventArgs> OnClickCallback { get; set; }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            base.BuildRenderTree(builder);
+            builder.OpenElement(5, "button");
+            builder.AddAttribute(6, "class", "btn btn-primary");
+            builder.AddAttribute(7, "onclick", OnClickCallback);
+            builder.AddContent(8, ButtonText);
+            builder.CloseElement();
+
+            //builder.AddMarkupContent(0, $"<button class=\"btn btn-primary\">{ButtonText}</button>");
+        }
+    }
+}
